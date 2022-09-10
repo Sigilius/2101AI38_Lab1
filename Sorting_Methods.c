7960b97996abcafdb748aeea15731225bcfbae7d
unknown <siddhant.senapati@gmail.com>
2022-09-11 00:07:54 +0530

    Added Selection Sort

--- Sorting_Methods.c
+++ Sorting_Methods.c
@@ -32,7 +32,23 @@ void swap(int *xp, int *yp)
     *yp = temp;
 }
   
-
+void selectionSort(int arr[], int n)
+{
+    int i, j, min_idx;
+  
+    // One by one move boundary of unsorted subarray
+    for (i = 0; i < n-1; i++)
+    {
+        // Find the minimum element in unsorted array
+        min_idx = i;
+        for (j = i+1; j < n; j++)
+          if (arr[j] < arr[min_idx])
+            min_idx = j;
+  
+        // Swap the found minimum element with the first element
+        swap(&arr[min_idx], &arr[i]);
+    }
+}
 
 
 int main()
@@ -51,6 +67,11 @@ int main()
 	case 1:
 	insertionSort(arr, n);
 	printf("Array after Insertion Sort is : \n");
+	printArray(arr, n);
+	break;
+	case 2:
+	selectionSort(arr,n);
+	printf("Array after Selection Sort is : \n");
 	printArray(arr, n);
 	break;
 

a0f5b930400b5d1686b14c5720c2877c00361ca5
unknown <siddhant.senapati@gmail.com>
2022-09-11 00:04:07 +0530

    Added 5 Operations

--- Calculator_S.c
+++ Calculator_S.c
@@ -4,7 +4,7 @@ int main() {
 
   int op;
   double first, second;
-  printf("Enter your choice 1. Addition 2. Subtraction 3. Multiplication 4. Division");
+  printf("Enter your choice 1. Addition 2. Subtraction 3. Multiplication 4. Division 5. Percentage");
   scanf("%d", &op);
   printf("Enter two operands: ");
   scanf("%lf %lf", &first, &second);
@@ -22,7 +22,11 @@ case 3:
  case 4:
       printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
       break;
+case 5:
+      printf("%.1lf is %.1lf% of %.1lf", first, (first / second)*100,second);
+      break;
     default:
+
       printf("Error! Option chosen is not correct");
   }
 
